e5b35748b253ceff75f49c9b6d6bef06754ca755
Lucas Adorno <lucasadorno16@hotmail.com>
2020-02-03 14:25:59 -0300

    Adicionado tratamento de erros no app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import cors from 'cors';
 import express from 'express';
+import 'express-async-errors';
+import { resolve } from 'path';
+import Youch from 'youch';
 import './database';
 import routes from './routes';
 
@@ -8,15 +11,34 @@ class App {
     this.server = express();
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
   middleware() {
     this.server.use(cors());
     this.server.use(express.json());
+
+    // Permite servir arquivos estáticos
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
 }
 
